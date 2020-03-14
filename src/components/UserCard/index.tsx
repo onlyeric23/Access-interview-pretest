4a99c89317a9df6a1c7f7f0d7c5e1cd33bac54c3
Eric <onlyeric23@gmail.com>
2020-03-14 17:41:56 +0800

    Add site_admin badge

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -1,18 +1,16 @@
 import React, { FC } from "react";
 import { UserCoarse } from "../../common/interface";
+import { GiPoliceBadge } from "react-icons/gi";
 
 import "./styles.scss";
 
-const UserRow: FC<UserCoarse> = ({
-  avatar_url,
-  login,
-  site_admin
-}) => {
+const UserRow: FC<UserCoarse> = ({ avatar_url, login, site_admin }) => {
   return (
     <div className="user-row">
       <img src={avatar_url} alt="" />
       <div className="info">
         <div className="name">{login}</div>
+        {site_admin && <GiPoliceBadge />}
       </div>
     </div>
   );
